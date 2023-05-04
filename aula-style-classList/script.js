1ff08d72d328141ffaf51734c03d333ad98cb1be
Nayara <nayarargs10@gmail.com>
2023-05-03 20:38:08 -0400

    atualizando

--- aula-style-classList/script.js
+++ aula-style-classList/script.js
@@ -11,13 +11,16 @@ usernameInput.classList.add("error")
 //SIMULANDO INTERAÇÃO DO USUÁRIO
 
 //usuário inseriu username errado
+//mudar a estilização do input e mensagem de erro do username sinalizando que ele errou
 usernameErroMessage.classList.add("visible")
 console.log("login-usuario")
 
 //usuário acertou o username mas errou a senha 
+//mudar estilização do input e mensagme de erro das tags de username sinalizando que ele acertou 
 usernameInput.classList.remove("error")
 usernameInput.classList.add("correct")
 usernameErroMessage.classList.remove("visible")
 
+//mudar estilização do input e mensagem de erro das tags  sinalizando que le errou 
 passwordInput.classList.add("error")
 passworMessage.classList.add("visible")
